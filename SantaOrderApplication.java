560f17548f68989ba8914e42eedac83d705b66f3
mattepu2024 <mahesh2024.cs@gmail.com>
2024-10-01 10:24:06 +0530

    Add files via upload

---
+++ SantaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.orders.cristmas.santa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaOrderApplication.class, args);
+	}
+
+}
